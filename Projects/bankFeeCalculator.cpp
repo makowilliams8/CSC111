a77d7068d2e09b7618d22ce3e63f1c925ab0ba75
Mako Williams <makowilliams5001@gmail.com>
2024-09-19 13:06:53 -0400

    Adding bank fee calculator project

---
+++ Projects/bankFeeCalculator.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double starting_balance;
+    int num_checks;
+    double check_fee;
+    double low_balance_fee;
+    double total_fees;
+    double new_balance;
+
+    cout << fixed << setprecision(2);
+
+    cout << "Please enter the starting balance: ";
+    cin >> starting_balance;
+    cout << "Please enter the number of checks: ";
+    cin >> num_checks;
+
+
+    if (starting_balance < 200) {
+        low_balance_fee = 10;
+    } else {
+        low_balance_fee = 0;
+    }
+
+    if (num_checks < 5) {
+        check_fee = num_checks * 0.15;
+    } else if (num_checks <= 10) {
+        check_fee = num_checks * 0.09;
+    } else if (num_checks <= 19) {
+        check_fee = num_checks * 0.07;
+    } else {
+        check_fee = num_checks * 0.05;
+    }
+
+    total_fees = low_balance_fee + check_fee + 7;
+
+    new_balance = starting_balance - total_fees;
+
+    cout << "Total fees: $" << total_fees << endl;
+    cout << "New balance: $" << new_balance << endl;
+
+    return 0;
+}
